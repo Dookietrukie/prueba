f9ea4ed7f511f36b0a4d1cbb1e82a60d446e259e
Cristian <cristianpalaocamarasa@gmail.com>
2021-03-11 13:34:29 +0100

    Nueva versión.
    He eliminado cosas innecesarias.
    
    Signed-off-by: Cristian <cristianpalaocamarasa@gmail.com>

--- GitTest/src/gitTest/Test.java
+++ GitTest/src/gitTest/Test.java
@@ -7,10 +7,6 @@ public class Test {
 		System.out.println("Hola");
 		System.out.println("Mundo.");
 		System.out.println("¿Qué tal?");
-		
-		for (int i = 0; i < args.length; i++) {
-			
-		}
 
 	}
 
