e82dee7c1fb976bea6ac80a14dc32d08e1946227
Cristian <cristianpalaocamarasa@gmail.com>
2021-03-11 13:30:04 +0100

    Nueva versión
    He añadido funcionalidad.
    
    Signed-off-by: Cristian <cristianpalaocamarasa@gmail.com>

--- GitTest/src/gitTest/Test.java
+++ GitTest/src/gitTest/Test.java
@@ -4,7 +4,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola mundo.");
+		System.out.println("Hola");
+		System.out.println("Mundo.");
+		System.out.println("¿Qué tal?");
+		
+		for (int i = 0; i < args.length; i++) {
+			
+		}
 
 	}
 
